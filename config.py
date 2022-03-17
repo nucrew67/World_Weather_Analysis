91b19167eba4c6bcd496698f0db51d821ee119ba
Randy Roger <rfamos53@gmail.com>
2022-03-16 21:09:47 -0700

    updating api_practice files.

---
+++ config.py
@@ -0,0 +1,6 @@
+# OpenWeather
+weather_api_key="cea9194e6f24c6e285120473fcaeb8f2"
+# NYT
+NYT_API_KEY = "NoWSpohoPn1wPhaaJJo2Aue3w9GWGuWZ"
+# Google
+Google_API_KEY = ""AIzaSyDPi7Nu5JOlfmnPrQTtAy9GsJzAzP_D-g8
